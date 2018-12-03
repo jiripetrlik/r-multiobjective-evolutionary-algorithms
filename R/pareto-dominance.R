473ec19daa88e71e5568f5cbca6f247ac839531b
Jiri Petrlik <jiripetrlik@gmail.com>
2018-12-03 23:17:13 +0100

    Pareto dominance relation utils

---
+++ R/pareto-dominance.R
@@ -0,0 +1,53 @@
+pareto_dominates_fast <- function(a, b) {
+  return(all(a <= b) && any(a < b))
+}
+
+#' @export
+pareto_dominates <- function(a, b, minimize = TRUE) {
+  if (is.numeric(a) == FALSE) {
+    stop("First parameter must be a numeric value")
+  }
+  if (is.numeric(b) == FALSE) {
+    stop("Second parameter must be a numeric value")
+  }
+  
+  if (minimize == FALSE) {
+    a <- (-a)
+    b <- (-b)
+  }
+  
+  return(pareto_dominates_fast(a, b))
+}
+
+check_objective_vectors_list <- function(l) {
+  if (any(sapply(l, function(x) { is.numeric(x) == FALSE } ))) {
+    stop("All items in objective vector list must be numeric vectors")
+  }
+  
+  if (length(l) > 0) {
+    size  <- length(l[[1]])
+    if (all(sapply(l, function(x) { length(x) == size })) == FALSE) {
+      stop("All vectors in objective vector list must have the same size")
+    }
+  }
+}
+
+is_nondominated_fast <- function(x, solutions, minimize) {
+  return(any(sapply(solutions, function(s) { pareto_dominates(s, x, minimize) })) == FALSE)
+}
+
+#' @export
+is_nondominated <- function(x, solutions, minimize = TRUE) {
+  check_objective_vectors_list(solutions)
+  return(is_nondominated_fast(x, solutions, minimize))
+}
+
+find_nondominated_fast <- function(solutions, minimize) {
+  return(sapply(solutions, function(s) {is_nondominated(s, solutions, minimize)}))
+}
+
+#' @export
+find_nondominated <- function(solutions, minimize = TRUE) {
+  check_objective_vectors_list(solutions)
+  return(find_nondominated_fast(solutions, minimize))
+}
---
+++ tests/testthat/test_pareto_dominance.R
@@ -0,0 +1,122 @@
+library(testthat)
+library(rmoea)
+
+context("Pareto dominance tests")
+
+test_that("Test Pareto dominance relation (minimize)", {
+  a <- c(2, 4, 8)
+  b <- c(5, 4, 8)
+  expect_true(pareto_dominates(a, b))
+  
+  a <- c(5, 1, 1)
+  b <- c(5, 4, 8)
+  expect_true(pareto_dominates(a, b))
+  
+  a <- c(5, 4, 8)
+  b <- c(5, 4, 8)
+  expect_false(pareto_dominates(a, b))
+  
+  a <- c(5, 4, 8)
+  b <- c(5, 1, 8)
+  expect_false(pareto_dominates(a, b))
+})
+
+test_that("Test Pareto dominance relation (maximize)", {
+  a <- c(2, 4, 8)
+  b <- c(5, 4, 8)
+  expect_false(pareto_dominates(a, b, minimize = FALSE))
+  
+  a <- c(5, 1, 1)
+  b <- c(5, 4, 8)
+  expect_false(pareto_dominates(a, b, minimize = FALSE))
+  
+  a <- c(5, 4, 8)
+  b <- c(5, 4, 8)
+  expect_false(pareto_dominates(a, b, minimize = FALSE))
+  
+  a <- c(5, 4, 8)
+  b <- c(5, 1, 8)
+  expect_true(pareto_dominates(a, b, minimize = FALSE))
+})
+
+test_that("Test check objective vectors list (positive scenario)", {
+  l <- list()
+  l[[1]] <- c(1, 5, 8, 3)
+  l[[2]] <- c(4, 5, 8, 7)
+  l[[3]] <- c(11, 5, 9, 2)
+  
+  expect_silent(check_objective_vectors_list(l))
+})
+
+test_that("Test check objective vectors list (non numeric vector)", {
+  l <- list()
+  l[[1]] <- c(1, 5, 8, 3)
+  l[[2]] <- c("Hello")
+  l[[3]] <- c(11, 5, 9, 2)
+  
+  expect_error(check_objective_vectors_list(l), "All items in objective vector list must be numeric vectors")
+})
+
+test_that("Test check objective vectors list (different length)", {
+  l <- list()
+  l[[1]] <- c(1, 5, 8, 3)
+  l[[2]] <- c(4, 5, 8)
+  l[[3]] <- c(11, 5, 9, 2)
+  
+  expect_error(check_objective_vectors_list(l), "All vectors in objective vector list must have the same size")
+})
+
+test_that("Test is non dominated (minimize)", {
+  l <- list()
+  l[[1]] <- c(5, 4, 3)
+  l[[2]] <- c(3, 4, 5)
+  l[[3]] <- c(4, 4, 4)
+  expect_true(is_nondominated(c(2, 2, 1), l))
+  expect_true(is_nondominated(c(5, 4, 3), l))
+  expect_false(is_nondominated(c(7, 4, 3), l))
+})
+
+test_that("Test is non dominated (maximize)", {
+  l <- list()
+  l[[1]] <- c(5, 4, 3)
+  l[[2]] <- c(3, 4, 5)
+  l[[3]] <- c(4, 4, 4)
+  expect_false(is_nondominated(c(2, 2, 2), l, minimize = FALSE))
+  expect_true(is_nondominated(c(5, 4, 3), l, minimize = FALSE))
+  expect_true(is_nondominated(c(7, 4, 3), l, minimize = FALSE))
+})
+
+test_that("Test is non dominated (bad vectors)", {
+  l <- list()
+  l[[1]] <- c(5, 4, 3)
+  l[[2]] <- c(3, 5)
+  l[[3]] <- c(4, 4, 4)
+  expect_error(is_nondominated(c(2, 2, 2), l, minimize = FALSE), "All vectors in objective vector list must have the same size")
+})
+
+test_that("Test find non dominated (minimize)", {
+  l <- list()
+  l[[1]] <- c(5, 4, 3)
+  l[[2]] <- c(8, 4, 3)
+  l[[3]] <- c(3, 4, 5)
+  l[[4]] <- c(4, 4, 4)
+  l[[5]] <- c(4, 4, 9)
+  expect_equal(find_nondominated(l), c(T, F, T, T, F))
+})
+
+test_that("Test find non dominated (maximize)", {
+  l <- list()
+  l[[1]] <- c(5, 4, 3)
+  l[[2]] <- c(8, 4, 3)
+  l[[3]] <- c(3, 4, 5)
+  l[[4]] <- c(4, 4, 4)
+  l[[5]] <- c(4, 4, 9)
+  expect_equal(find_nondominated(l, minimize = FALSE), c(F, T, F, F, T))
+})
+
+test_that("Test find non dominated (bad vectors)", {
+  l <- list()
+  l[[1]] <- c(5, 4, 3)
+  l[[2]] <- c("Hello")
+  expect_error(find_nondominated(l), "All items in objective vector list must be numeric vectors")
+})
