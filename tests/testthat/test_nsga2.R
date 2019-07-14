d9ddfb6fa45db549402fcb41e9b501173033da20
Jiri Petrlik <jiripetrlik@gmail.com>
2019-07-14 13:59:40 +0200

    NSGAII - main loop of binary variant

--- tests/testthat/test_nsga2.R
+++ tests/testthat/test_nsga2.R
@@ -51,3 +51,55 @@ test_that("Crowding distance assignment - multiple solutions", {
   expect_equal(length(result), 5)
   expect_equal(result, c(1.2, Inf, 1.8, Inf, 1.8))
 })
+
+test_that("Test binary NSGAII", {
+  set.seed(1)
+  
+  number_of_iterations = 100
+  f1 <- function(ch) {
+    x <- binary_chromosome_to_numeric(ch, -10, 10)
+    return(abs(x + 0.1))
+  }
+  f2 <- function(ch) {
+    x <- binary_chromosome_to_numeric(ch, -10, 10)
+    return(abs(x + 0.2))
+  }
+  functions <- list(f1, f2)
+  
+  results <- nsga2(functions, number_of_iterations = number_of_iterations, chromosome_size = 24, chromosome_type = "binary")
+  expect_gt(nrow(results$values), 2)
+  expect_lt(min(results$values[, 1]), 0.02)
+  expect_lt(min(results$values[, 2]), 0.02)
+})
+
+test_that("Test binary NSGAII parameters", {
+  f1 <- function(ch) {
+    x <- binary_chromosome_to_numeric(ch, -10, 10)
+    return(abs(x + 0.1))
+  }
+  f2 <- function(ch) {
+    x <- binary_chromosome_to_numeric(ch, -10, 10)
+    return(abs(x + 0.2))
+  }
+  functions <- list(f1, f2)
+  
+  chromosome_size <- 16
+  chromosome_type <- "binary"
+  population_size <- 50
+  number_of_iterations <- 10
+  mutation_probability <- 0.1
+  
+  results <- nsga2(functions,
+                  chromosome_size = chromosome_size,
+                  chromosome_type = chromosome_type,
+                  population_size = population_size,
+                  number_of_iterations = number_of_iterations,
+                  mutation_probability = mutation_probability);
+  
+  expect_length(results$parameters$objective_functions_list, 2)
+  expect_equal(results$parameters$chromosome_size, chromosome_size)
+  expect_equal(results$parameters$chromosome_type, chromosome_type)
+  expect_equal(results$parameters$population_size, population_size)
+  expect_equal(results$parameters$number_of_iterations, number_of_iterations)
+  expect_equal(results$parameters$mutation_probability, mutation_probability)
+})
