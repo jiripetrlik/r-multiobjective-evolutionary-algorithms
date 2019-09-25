fa570a4d22cc8a431a7ba4323a754c54260efa25
Jiri Petrlik <jiripetrlik@gmail.com>
2019-09-25 23:54:26 +0200

    Benchmark functions

--- R/benchmarks.R
+++ R/benchmarks.R
@@ -2,3 +2,38 @@
 quadratic_benchmark_function <- function(x) {
   return(sum(x^2))
 }
+
+#' @export
+fonseca_fleming_function <- function(v) {
+  n <- 1:length(v)
+  value1 <- 1 - exp(-sum((v - 1 / sqrt(n))^2))
+  value2 <- 1 - exp(-sum((v + 1 / sqrt(n))^2))
+
+  return(c(value1, value2))
+}
+
+#' @export
+kursawe_function <- function(v) {
+  if (length(v) != 3) {
+    stop("Kursawe function must have 3 parameters")
+  }
+
+  tmpV1 <- v[1:2]
+  tmpV2 <- v[2:3]
+  value1 <- sum(-10 * exp(-0.2 * sqrt(tmpV1^2 + tmpV2^2)))
+  value2 <- sum(abs(v)^0.8 + 5 * sin(v^3))
+
+  return(c(value1, value2))
+}
+
+#' @export
+schaffer_function <- function(x) {
+  if (length(x) != 1) {
+    stop("Schaffer function must have only 1 parameter")
+  }
+
+  value1 <- x^2
+  value2 <- (x - 2)^2
+
+  return(c(value1, value2))
+}
